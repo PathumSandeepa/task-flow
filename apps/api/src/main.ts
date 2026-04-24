71e972cc46311f2a89500a88acd1e3eca04e9982
PathumSandeepa <pathumsandeepa13@gmail.com>
2026-04-24 20:34:07 +0530

    [task-04] update api entrypoint with cors and prefix configuration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 void bootstrap();
